7c2f85cfa27a8948dab7ac88b64bd506acc73aba
geekbrains <info@geekbrains.ru>
2020-09-16 18:42:04 +0300

    ДЗ к уроку 7 изменения

--- src/main/java/Lesson7Test.java
+++ src/main/java/Lesson7Test.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Lesson7Test {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        start(Lesson7Test.class);
+        start(TestedMethods.class);
     }
 
     public static void start(Class c) throws InvocationTargetException, IllegalAccessException {
@@ -56,53 +56,5 @@ public class Lesson7Test {
             arrAfter.get(0).invoke(Lesson7Test.class);
 
     }
-
-    @BeforeSuite
-    public static void before(){
-        System.out.println("Начинаем тестирование\n");
-    }
-
-
-    @AfterSuite
-    public static void after(){
-        System.out.println("Конец тестировани\n");
-    }
-
-
-    @Test(priority = 1)
-    public static void test1(){
-        System.out.println("Тест 1 приоритет 1\n");
-    }
-
-    @Test(priority = 10)
-    public static void test2(){
-        System.out.println("Тест 2 приоритет 10\n");
-    }
-
-    @Test(priority = 3)
-    public static void test3(){
-        System.out.println("Тест 3 приоритет 3\n");
-    }
-
-    @Test
-    public static void test4(){
-        System.out.println("Тест 4 приоритет 5\n");
-    }
-
-    @Test(priority = 8)
-    public static void test5(){
-        System.out.println("Тест 5 приоритет 8\n");
-    }
-
-    @Test(priority = 10)
-    public static void test6(){
-        System.out.println("Тест 6 приоритет 10\n");
-    }
-
-    @Test(priority = 3)
-    public static void test7(){
-        System.out.println("Тест 7 приоритет 3\n");
-    }
-
 
 }
